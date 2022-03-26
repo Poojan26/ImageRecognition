372cea3419f18d05ed8d681d648dba07f794f5ff
Poojan Patel <poojan.patel1126@gmail.com>
2022-03-26 13:43:57 -0700

    Files added

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Jay on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
